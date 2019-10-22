b496dd6432bf7b23204bd6bf0374f6221e3f8eca
Bruno <bruno_lemes@outlook.com>
2019-10-21 22:33:35 -0300

    ajuste Editar

--- src/Fiap.MasterChefReceitas.Web/Models/NovoReceitaViewModel.cs
+++ src/Fiap.MasterChefReceitas.Web/Models/NovoReceitaViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -20,6 +21,9 @@ namespace Fiap.MasterChefReceitas.Web.Models
         public string IngredientesTexto { get; set; }
 
         public List<IngredienteViewModel> Ingredientes { get; set; }
+
+        [NotMapped]
+        public string Preparo { get; set; }
 
         public PreparoViewModel Preparos { get; set; }
     }
